5fcd6b752949a7bbac86ddc2fc3ea097792b45de
kristoffer-Johnsen <141549801+kristoffer-Johnsen@users.noreply.github.com>
2024-01-08 14:59:48 +0100

    .

---
+++ ConsoleCollection/Clicker/baseClicker game/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baseClicker_game
+{
+    internal class Game
+    {
+        private string _playername;
+        private int _score = 0;
+        private int _clicks = 0;
+        private int _upgrades = 1;
+        private double _feeincrease = 1.2;
+
+        public Game(string name)
+        {
+            _playername = name;
+        }
+
+        void start(Game game)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(0,3);
+                Console.WriteLine($"Score: {_score}");
+            }
+        }
+    }
+
+}
---
+++ ConsoleCollection/Clicker/baseClicker game/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace baseClicker_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                var wpfWindow = new ClickerWindow.MainWindow();
+                wpfWindow.ShowDialog();
+            });
+
+
+
+
+
+            string? username = null;
+            while (string.IsNullOrEmpty(username))
+            {
+                username = Console.ReadLine();
+                Console.WriteLine("name not viable, please try again");
+            }
+
+            Game game = new(username);
+            
+        }
+    }
+
+   
+}
